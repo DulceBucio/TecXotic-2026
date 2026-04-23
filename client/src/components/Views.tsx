9a2113c8077f714a88d628f031faf21b1b1c802e
Carolina Figueroa <andreacarolina.f28@gmail.com>
2026-04-22 18:27:58 -0600

    Model components and 3d model for testing

--- client/src/components/Views.tsx
+++ client/src/components/Views.tsx
@@ -1,10 +1,13 @@
 import { Routes, Route } from 'react-router-dom'
 import Main from '../pages/Main/Main'
+import Model from '../pages/Model/Model'
 
 const Views = () => {
     return (
         <Routes>
+            <Route path='/model' element={<Model/>}></Route>
             <Route path='/' element={<Main/>}></Route>
+            
         </Routes>
     )
 }
